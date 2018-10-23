4173239b8b969b5e9c556fe9f208d50ad3be1d0f
adamkzh <kzh88@hotmail.com>
2018-10-22 17:21:20 -0700

    add background & first two steps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+var CANVAS_WIDTH = window.innerWidth;
+var CANVAS_HEIGHT = window.innerHeight;
+    
+var canvas;
+var ctx;
+
+var img = new Image();
+img.src = "http://www.clipartbest.com/cliparts/nTB/pK7/nTBpK7GTA.svg";
+img.width = img.width *0.02;
+img.height = img.height *0.02;
+
 class App extends Component {
+  componentDidMount() {
+    canvas = document.getElementById('canvas');
+    if (canvas && canvas.getContext) {
+      ctx = canvas.getContext('2d');
+      canvas.width = CANVAS_WIDTH;
+      canvas.height = CANVAS_HEIGHT;
+      this.step();
+    }
+  }
+  step = () =>{
+    console.log( window.innerHeight);
+    console.log(CANVAS_HEIGHT);
+    ctx.drawImage(img,160,120,img.width, img.height);
+    ctx.drawImage(img,150,100,img.width, img.height);
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <canvas id='canvas'></canvas>
       </div>
     );
   }
